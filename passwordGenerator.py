a6f6fe74a5d6e3e7452c46a052a737deef4ec09a
ndolny <nathan.dolny@outlook.com>
2020-12-20 21:29:27 -0600

    version 1.2

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -6,7 +6,7 @@ letters = string.ascii_letters
 numbers = string.digits
 scharacters = string.punctuation
 
-passwordLength = input("Length of required password: ")
+passwordLength = input("Enter length of password: ")
 password = "".join(random.choice(letters+numbers+scharacters) for i in range (passwordLength))
  
 print("Your new password: " + password) 
